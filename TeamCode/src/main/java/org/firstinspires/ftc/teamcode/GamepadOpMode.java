28d5c97d513bd6c085f022e0b9da1f168aa75c8f
rayam <ftc19427@gmail.com>
2023-09-16 19:20:31 -0400

    added file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadOpMode.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@TeleOp()
+
+public class GamepadOpMode extends OpMode{
+
+    @Override
+    public void init() {
+    }
+    double squareInputWithSign(double input){
+        double output = input * input;
+        if(input < 0){
+            output = output * -1;
+        }
+        return output;
+    }
+
+    @Override
+    public void loop() {
+        double leftAmount = gamepad1.left_stick_x;
+        double fwdAmount = -gamepad1.left_stick_y;
+
+        telemetry.addData("Before X", leftAmount);
+        telemetry.addData("Before Y", fwdAmount);
+
+        leftAmount = squareInputWithSign(leftAmount);
+        fwdAmount = squareInputWithSign(fwdAmount);
+
+        telemetry.addData("After X", leftAmount);
+        telemetry.addData("After Y", fwdAmount);
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingBoard2.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+//disable
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp()
+
+public class ProgrammingBoard2 {
+    private DigitalChannel touchSensor;
+    private DcMotor motor;
+    private DcMotor motor1;
+    private DcMotor motor2;
+    private DcMotor motor3;
+    private double ticksPerRotation;
+
+    public void init(HardwareMap hwMap) {
+        touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
+        touchSensor.setMode(DigitalChannel.Mode.INPUT);
+        motor = hwMap.get(DcMotor.class, "motor");
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor1 = hwMap.get(DcMotor.class, "motor1");
+        motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor2 = hwMap.get(DcMotor.class, "motor2");
+        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor3 = hwMap.get(DcMotor.class, "motor3");
+        motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ticksPerRotation = motor.getMotorType().getTicksPerRev();
+    }
+    public boolean getTouchSensorState() {
+        return touchSensor.getState();
+    }
+    public void setMotorSpeed(double speed) {
+        motor.setPower(-speed);
+        motor1.setPower(speed);
+        motor2.setPower(-speed);
+        motor3.setPower(speed);
+    }
+    public double getMotorRotations(){
+        return motor.getCurrentPosition() / ticksPerRotation;
+    }
+}

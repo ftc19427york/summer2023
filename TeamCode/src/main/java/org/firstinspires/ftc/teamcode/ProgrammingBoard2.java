bfe49cc304b71e1e2683978af39a120eec6f4f85
Ryan Ames <ryanames3@gmail.com>
2023-09-17 13:19:03 -0400

    added april tag based movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingBoard2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingBoard2.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+
 
 @TeleOp()
 

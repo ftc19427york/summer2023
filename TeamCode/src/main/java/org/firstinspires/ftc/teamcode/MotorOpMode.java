46004145bbfc9395cd5ab21e35c7e7a513dd1ac7
rayam <ftc19427@gmail.com>
2023-10-01 19:51:58 -0400

    Merge remote-tracking branch 'origin/master'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorOpMode.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.ProgrammingBoard1;
-
+@Disabled
 @TeleOp()
 public class MotorOpMode extends OpMode {
     ProgrammingBoard1 board = new ProgrammingBoard1();
